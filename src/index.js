57bc78c387c2825f53dcdbc943cf85331940f354
msosborne3 <msosborne@ymail.com>
2017-04-19 15:13:16 -0500

    Add Provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import listReducer from './reducers/listReducer';
 
 const store = createStore(listReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <App store={store} />,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );

146739ed35a973d27e97f3bb2bed711bc200ed0b
msosborne3 <msosborne@ymail.com>
2017-04-18 20:07:00 -0500

    Initialize emtpy react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DumbComponent from './components/DumbComponent';
+import DumberComponent from './components/DumberComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DumbComponent />
+        <DumberComponent />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <h1>Hello!!</h1>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

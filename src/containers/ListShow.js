06d1bad09eb097fc90dcc88a7620ff340dff2552
msosborne3 <msosborne@ymail.com>
2017-04-20 15:14:06 -0500

    Refactor ListShow page to display items from store rather than manually

--- src/containers/ListShow.js
+++ src/containers/ListShow.js
@@ -1,23 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleDone } from '../actions/index'
+import { bindActionCreators } from 'redux';
 
 const ListShow = (props) => {
-  let list = {id: 1, name: 'school', items: [{id: 1, name: 'hw 1', description: 'Algebra', timeDue: 'Tomorrow', location: 'School', done: false}, {id: 2, name: 'hw 4', description: 'Chemistry', timeDue: 'Thursday', location: 'School', done: true}]};
+  let list = {id: 1, name: 'school'};
+  let items = [{id: 1, text: 'hw1', done: false, parentID: 1}, {id: 2, text: 'hw2', done: false, parentID: 1}, {id: 3, text: 'workout', done: false, parentID: 2}, {id: 4, text: 'hw3', done: false, parentID: 1}, {id: 5, text: 'run', done: false, parentID: 5}]
+  let listItems = items.filter(item => item.parentID === list.id);
 
   return (
     <div>
-      <h2>{list.name}</h2>
+      <h2>List: {list.name}</h2>
 
-      {list.items.map((item) => 
-        <div>
-          <p><strong>{item.name}</strong></p>
-          <p>{item.done ? "Done" : "Not Done"}</p>
-          <p>Description: {item.description}</p>
-          <p>Time Due: {item.timeDue}</p>
-          <p>Location: {item.location}</p>
-        </div>
-      )}
+      <ul>
+        {listItems.map((item) => 
+          <li key={item.id} onClick={() => {this.props.toggleDone(item)}}>{item.done ? <strike>{item.text}</strike> : item.text}</li>
+        )}
+      </ul>
     </div>
   );
 };
+
+/*
+const mapStateToProps = (state, ownProps) => {
+  const list = state.lists.find( (list) => list.id === ownProps.routeParams.id )
+  if (list) {
+    return {
+      list: list,
+      items: state.items
+    }
+  } else {
+    return {
+      list: {},
+      items: []
+    };
+  }
+};*/
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleDone: bindActionCreators(toggleDone, dispatch)
+  };
+}
+
+//export default connect(mapStateToProps, mapDispatchToProps)(ListShow);
+
 
 export default ListShow;

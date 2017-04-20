deb25c9b24dd404adbb10c7ff53ee3a97c1bd0bc
msosborne3 <msosborne@ymail.com>
2017-04-20 18:12:50 -0500

    Include routing in index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,31 @@ import App from './App';
 import './index.css';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import listReducer from './reducers/listReducer';
-import itemsReducer from './reducers/itemsReducer';
 import rootReducer from './reducers'
+//Routing:
+import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import ListPage from './containers/ListPage';
+import ListShow from './containers/ListShow';
+import ListNew from './containers/ListNew';
+import ItemsNew from './containers/ItemsNew';
 
-//const rootReducer = combineReducers({listReducer, itemsReducer});
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store} />
+    <App store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/lists' component={ListPage} />
+          <Route exact path='/lists/new' component={ListNew} />
+          <Route exact path='/lists/:id' component={ListShow} />
+          <Route exact path='/items/new' component={ItemsNew} />
+        </Switch>
+      </BrowserRouter>
+    </App>
   </Provider>,
   document.getElementById('root')
 );

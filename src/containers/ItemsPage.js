2bb5940e765ca285a1f4b07c9872e70372d272bb
msosborne3 <msosborne@ymail.com>
2017-04-20 00:29:18 -0500

    Render text for each item

--- src/containers/ItemsPage.js
+++ src/containers/ItemsPage.js
@@ -8,7 +8,7 @@ class ItemsPage extends Component {
         <h2>To-do Items</h2>
         <ul>
           {this.props.items.map((item) => 
-            <li key={item.id}>{item.name}</li>
+            <li key={item.id}>{item.text}</li>
           )}
         </ul>
       </div>

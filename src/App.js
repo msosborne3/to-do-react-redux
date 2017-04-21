3bb0cd50109fedb941655db0161e63e0f665bca0
msosborne3 <msosborne@ymail.com>
2017-04-21 16:01:35 -0500

    Remove testing components and add routing information

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ItemsNew from './containers/ItemsNew';
-import ListNew from './containers/ListNew';
 import ListPage from './containers/ListPage';
-import ItemsPage from './containers/ItemsPage';
-import ListShow from './containers/ListShow';
 
-class App extends Component {
+
+const App = props => ({
   render() {
     return (
       <div className="App">
-
-        <ListNew store={this.props.store} />
-        <ListPage store={this.props.store} />
-        
-
-        <ItemsNew store={this.props.store} />
-        <ItemsPage store={this.props.store} />
-
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to your to do list!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ListPage store={props.store} />
+        {props.children}
       </div>
     );
   }
-}
+});
 
 export default App;
